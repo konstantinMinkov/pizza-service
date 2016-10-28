a80016d3e76df036b1ee012b1e73f2c6c0a3bcd7
Kostiantyn_Minkov <zakedystyle@gmail.com>
2016-10-28 17:40:32 +0300

    add tests

--- src/test/java/ua/rd/pizzaservice/repository/JpaPizzaRepositoryTestIT.java
+++ src/test/java/ua/rd/pizzaservice/repository/JpaPizzaRepositoryTestIT.java
@@ -3,6 +3,8 @@ package ua.rd.pizzaservice.repository;
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ua.rd.pizzaservice.domain.Pizza;
 import ua.rd.pizzaservice.domain.PizzaType;
 
@@ -25,14 +27,43 @@ public class JpaPizzaRepositoryTestIT extends RepositoryTestConfig {
 
     @Test
     public void find() {
-//        Pizza pizza = new Pizza(2L, )
-//        jdbcTemplate.update()
+        final Pizza pizza = new Pizza(2L, "Margarita", 100L, PizzaType.SEA);
+        jdbcTemplate.update("insert into Pizza values(?, ?, ?, ?)",
+                pizza.getId(),
+                pizza.getName(),
+                pizza.getPrice(),
+                pizza.getType().toString());
+        assertThat(repository.findById(2L), is(pizza));
     }
 
     @Test
     public void save() {
         final Pizza pizza = new Pizza("Pizza", 100L, PizzaType.SEA);
-        final Pizza savedPizza = repository.save(pizza);
+        final Pizza savedPizza = savePizza(pizza);
+        final Pizza dbPizza = jdbcTemplate.queryForObject(
+                "select * from Pizza where id = ?",
+                new Object[] { savedPizza.getId() },
+                (rs, i) -> {
+                    Pizza pizza1 = new Pizza();
+                    pizza1.setId(rs.getLong("id"));
+                    pizza1.setName(rs.getString("name"));
+                    pizza1.setPrice(rs.getLong("price"));
+                    pizza1.setType(PizzaType.valueOf(rs.getString("type")));
+                    return pizza1;
+                });
+
+        String name = (String) jdbcTemplate.queryForObject(
+                "select name from Pizza where name = 'Pizza'",
+                String.class
+        );
+        System.out.println(name);
+
         assertThat(savedPizza.getId(), is(notNullValue()));
+//        assertThat(savedPizza, is(dbPizza));
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    private Pizza savePizza(Pizza pizza) {
+        return repository.save(pizza);
     }
 }
--- src/test/java/ua/rd/pizzaservice/services/SimplePizzaServiceTest.java
+++ src/test/java/ua/rd/pizzaservice/services/SimplePizzaServiceTest.java
@@ -21,16 +21,12 @@ public class SimplePizzaServiceTest {
     public static void createService() {
         final InMemoryPizzaRepository repository = mock(InMemoryPizzaRepository.class);
         when(repository.findById(0L)).thenReturn(new Pizza(0L, "Pizza 1", 240L, PizzaType.MEAT));
-        when(repository.findById(1L)).thenReturn(new Pizza(1L, "Pizza 2", 140L, PizzaType.SEA));
-        when(repository.findById(2L)).thenReturn(new Pizza(2L, "Pizza 3", 350L, PizzaType.VEGETARIAN));
         service = new SimplePizzaService(repository);
     }
 
     @Test
     public void testFindById() throws Exception {
         assertThat(service.findById(0L), is(new Pizza(0L, "Pizza 1", 240L, PizzaType.MEAT)));
-        assertThat(service.findById(1L), is(new Pizza(1L, "Pizza 2", 140L, PizzaType.SEA)));
-        assertThat(service.findById(2L), is(new Pizza(2L, "Pizza 3", 350L, PizzaType.VEGETARIAN)));
         assertThat(service.findById(3L), is(nullValue()));
     }
 }
