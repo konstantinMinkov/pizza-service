ad5e4f14273927412723253e69ece188abae3edc
Kostiantyn_Minkov <zakedystyle@gmail.com>
2016-11-10 13:05:12 +0200

    update PizzaRestController class

--- src/main/java/ua/rd/pizzaservice/web/PizzaRestController.java
+++ src/main/java/ua/rd/pizzaservice/web/PizzaRestController.java
@@ -2,9 +2,11 @@ package ua.rd.pizzaservice.web;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import ua.rd.pizzaservice.domain.Pizza;
 import ua.rd.pizzaservice.services.PizzaService;
 
@@ -19,8 +21,27 @@ public class PizzaRestController {
         this.pizzaService = pizzaService;
     }
 
-    @RequestMapping("/hello/{id}")
-    public Pizza getPizza(@PathVariable("id") Long id) {
-        return pizzaService.findById(id);
+    @RequestMapping(value = "/pizza/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Pizza> find(@PathVariable("id") Long id) {
+        final Pizza pizza = pizzaService.findById(id);
+        if (pizza == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(pizza, HttpStatus.FOUND);
+    }
+
+    @RequestMapping(value = "/pizza", method = RequestMethod.POST)
+    public ResponseEntity<Pizza> save(@RequestBody Pizza pizza,
+                                      UriComponentsBuilder builder) {
+        Pizza p = pizzaService.save(pizza);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(
+                builder.path("/pizza/{id}")
+                        .buildAndExpand(p.getId())
+                        .toUri()
+        );
+
+        return new ResponseEntity<Pizza>(headers, HttpStatus.CREATED);
     }
 }
